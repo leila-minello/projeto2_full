1195ca8e40d378526c9ee845f30706f42119ce91
Gabvfla <miyatinho@gmail.com>
2025-02-10 16:11:31 -0300

    configurações iniciais

--- servidor - back/app.js
+++ servidor - back/app.js
@@ -1,28 +1,24 @@
 const express = require('express');
-const cors = require ('cors');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-dotenv.config();
+dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//middlewares
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 
-//banco de dados
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('conectado ao banco de dados MongoDB!'))
-  .catch(err => console.error('erro ao conectar ao banco de dados MongoDB:', err));
+  .then(() => console.log('Conectado ao MongoDB'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
-//rota de teste
 app.get('/', (req, res) => {
-    res.send('o servidor está funcionando!!');
-  });
+  res.send('Backend está funcionando!');
+});
 
-//iniciando servidor
 app.listen(PORT, () => {
-    console.log('servidor rodando na porta ${PORT}');
-})
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
