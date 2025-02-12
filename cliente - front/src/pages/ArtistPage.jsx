7a24bb0951383f78531ecc55eccc8535d251d7ef
leila <132851093+leila-minello@users.noreply.github.com>
2025-02-12 14:22:49 -0300

    refatorando a ArtistPage para ser uma página de inserção

--- cliente - front/src/pages/ArtistPage.jsx
+++ cliente - front/src/pages/ArtistPage.jsx
@@ -1,103 +1,122 @@
-import React, { useEffect, useState, useMemo } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { Box, Typography, Card, CardContent, CardMedia, Button, Grid } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Typography, TextField, Button } from "@mui/material";
 
 const ArtistPage = () => {
-  const { artistName } = useParams();
-  const [artist, setArtist] = useState(null);
-  const [tracks, setTracks] = useState([]);
+  const [name, setName] = useState("");
+  const [favSong, setFavSong] = useState("");
+  const [genre, setGenre] = useState("");
+  const [popularity, setPopularity] = useState("");
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  uuseEffect(() => {
-    const fetchTracks = async () => {
-      try {
-        const response = await fetch(`http://localhost:5000/api/artists/${artistId}/tracks`, {
-          method: 'GET',
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`,
-          },
-        });
-        const data = await response.json();
-        setTracks(data);
-      } catch (err) {
-        setError('Erro ao buscar músicas.');
-      }
-    };
-  
-    fetchTracks();
-  }, [artistId]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const memoizedTracks = useMemo(() => tracks, [tracks]);
+    const token = localStorage.getItem("token");
+
+    if(!token) {
+      setError("Usuário não autenticado!");
+      return;
+    }
+
+    const artistData = { name, favSong, genre, popularity: Number(popularity)};
+
+    try {
+      const response = await fetch("http://localhost:5000/api/artists", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(artistData),
+      });
+
+      if(response.ok) {
+        alert("Artista cadastrado com sucesso!!");
+        navigate("/");
+      } else {
+        setError(data.message || "Erro ao cadastrar o artista..");
+      }
+    } catch (err){
+      setError("Erro de conexão.");
+    }
+  };
 
   return (
-    <Box sx={{ 
-      fontFamily: "National",
-      textAlign: "center", 
-      mt: 5,
-      color: "white",
-      backgroundColor: "#d92323",
-      padding: "120px",
-      borderRadius: "10px" }}>
+    <Box 
+      sx={{ 
+        fontFamily: "National",
+        textAlign: "center", 
+        mt: 5,
+        color: "white",
+        backgroundColor: "#d92323",
+        padding: "120px",
+        borderRadius: "10px",
+        maxWidth: "500px",
+        margin: "auto", }}>
+        <Typography variant="h4" sx={{ mb: 3 }}>
+          Adicionar novo artista
+        </Typography>
+
       {error && <Typography color="error">{error}</Typography>}
 
-      {artist && (
-        <Box>
-          <Typography variant="h4" sx={{ mb: 2 }}>
-            Artista: {artist}
-          </Typography>
-          <Typography variant="h6" sx={{ mb: 4 }}>
-            Principais Músicas
-          </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Nome do artista"
+          fullWidth
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          sx={{ mb:2, backgroundColor: "white", borderRadius: "5px"}}
+        />
+        <TextField
+          label="Música preferida"
+          fullWidth
+          required
+          value={favSong}
+          onChange={(e) => setFavSong(e.target.value)}
+          sx={{ mb:2, backgroundColor: "white", borderRadius: "5px"}}
+        />
+        <TextField
+          label="Gênero das músicas"
+          fullWidth
+          required
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+          sx={{ mb:2, backgroundColor: "white", borderRadius: "5px"}}
+          
+        />
+        <TextField
+          label="Popularidade (1-10)"
+          fullWidth
+          required
+          value={popularity}
+          onChange={(e) => setPopularity(e.target.value)}
+          sx={{ mb:2, backgroundColor: "white", borderRadius: "5px"}}
+          
+        />
 
-          <Grid container spacing={4} justifyContent="center">
-            {memoizedTracks.map((track) => (
-              <Grid item xs={12} sm={6} md={4} key={track.name}>
-                <Card sx={{ maxWidth: 345, boxShadow: 3, borderRadius: 2 }}>
-                  <CardMedia
-                    component="img"
-                    alt={track.name}
-                    height="180"
-                    image={track.albumImage}
-                    sx={{ objectFit: "cover" }}
-                  />
-                  <CardContent sx={{ backgroundColor: "#fafafa", padding: "1.5rem" }}>
-                    <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                      {track.name}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      Artista: {track.artist}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{
-              display: "block",
-              margin: "50px auto",
-              padding: "0.8rem 2rem",
-              fontWeight: "bold",
-              borderRadius: "8px",
-              backgroundColor: "black",
-              transition: "background-color 0.3s",
-              "&:hover": {
-                backgroundColor: "white",
-                color: "black"
-          },
-            }}
-            onClick={() => navigate(-1)}
-          >
-            Voltar
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{
+            mt: 2,
+            padding: "10px 20px",
+            fontWeight: "bold",
+            backgroundColor: "black",
+            "&:hover": {
+              backgroundColor: "white",
+              color: "black",
+            },
+          }}>
+            Salvar artista
           </Button>
-        </Box>
-      )}
+      </form>
     </Box>
   );
 };
+      
 
 export default ArtistPage;
--- servidor - back/models/Artist.js
+++ servidor - back/models/Artist.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const artistSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  favSong: { type: String, required: true},
   genre: { type: String, required: true },
   popularity: { type: Number, default: 0 },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
